9763d40a543b4b384aff4f846b00e4e54f92e003
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2023-11-21 11:07:06 -0300

    Criacao das tabelas

---
+++ javaapi/src/main/java/com/felipe/javaapi/model/UserModel.java
@@ -0,0 +1,46 @@
+package com.felipe.javaapi.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Document("user")
+public class UserModel {
+    @Id
+    private String id;
+    private String name;
+    private String dataNasc;
+    private String sex;
+    private String cpf;
+    private String bloco;
+    private String apto;
+    private String email;
+    private String password;
+    private boolean adm;
+    private short vaga;
+
+    public UserModel(String name, String dataNasc, String sex, String cpf, String bloco, String email, String apto, String password, Boolean adm, short vaga)
+    {
+        this.name = name;
+        this.dataNasc = dataNasc;
+        this.sex = sex;
+        this.cpf = cpf;
+        this.bloco = bloco;
+        this.apto = apto;
+        this.email = email;
+        this.password = password;
+        this.adm = adm;
+        this.vaga = vaga;
+    }
+
+    public UserModel(String id)
+    {
+        this.id = id;
+    }
+
+    public UserModel() { }
+}
